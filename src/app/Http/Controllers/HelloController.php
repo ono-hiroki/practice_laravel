cc18ba710f0fb9c97555caf0be5bdda639e4a053
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-28 11:20:06 +0900

    コントローラーとviewで適当に

---
+++ src/app/Http/Controllers/HelloController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+    public function index(Request $request, Response $response)
+    {
+        $data = [
+            'msg' => '',
+        ];
+        return view('hello.index', $data);
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = [
+            'msg' => $msg . 'さん！',
+        ];
+        return view('hello.index', $data);
+    }
+}
---
+++ src/resources/views/hello/index.blade.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+    <title>Hello/Index</title>
+    <style>
+        body {
+            font-size: 16pt;
+            color: #999;
+        }
+
+        h1 {
+            font-size: 100pt;
+            text-align: right;
+            color: #f6f6f6;
+            margin: -50px 0px -100px 0px;
+        }
+    </style>
+</head>
+<body>
+<h1>Index</h1>
+<p>This is a sample page with php-template.</p>
+@if($msg != '')
+    <p>こんにちは、{{$msg}}さん。</p>
+@else
+    <p>何か書いてください。</p>
+@endif
+<form method="POST" action="/hello">
+    {{ csrf_field() }}
+    <input type="text" name="msg">
+    <input type="submit">
+</form>
+
+</body>
+</html>
