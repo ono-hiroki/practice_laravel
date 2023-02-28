e2999320bc4c05ab8447b9a7a88ca90864a8896b
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-28 19:36:00 +0900

    ミドルウェアの試し

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -10,28 +10,11 @@
 @section('content')
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
-    {{--    @component('components.message')--}}
-    {{--        @slot('msg_title')--}}
-    {{--            CAUTION!--}}
-    {{--        @endslot--}}
-
-    {{--        @slot('msg_content')--}}
-    {{--            これはメッセージの表示です。--}}
-    {{--        @endslot--}}
-    {{--    @endcomponent--}}
     @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビューです。'])
-    @each('components.item', $data, 'item')
-    <p>Controller value<br>'view_message' = {{$view_message}}</p>
+    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
+    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
 
-    @foreach($data as $item)
-        @if($loop->first)
-            <p>※データ一覧</p><ul>
-        @endif
-        <li>No,{{$loop->iteration}}. {{$item['name']}}</li>
-        @if($loop->last)
-            </ul><p>ここまで</p>
-        @endif
-    @endforeach
+
 
 @endsection
 
