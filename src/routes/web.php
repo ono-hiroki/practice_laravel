79956511048d9ee19eaf666449208b637098ab4d
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-28 11:13:23 +0900

    cacheクリアコマンド修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', [HelloController::class, 'index']);
+Route::post('/hello', [HelloController::class, 'post']);
