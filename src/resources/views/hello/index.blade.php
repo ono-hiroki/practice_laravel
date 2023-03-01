3e124bad8c0596f0e22b9170db7d77df83c4bd06
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-01 10:00:10 +0900

    簡単にバリデーション

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -11,9 +11,46 @@
     <p>ここが本文のコンテンツです。</p>
     <p>必要なだけ記述できます。</p>
     @include('components.message', ['msg_title'=>'OK', 'msg_content'=>'サブビューです。'])
-    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+    <p>これは、
+        <middleware>google.com</middleware>
+        へのリンクです。
+    </p>
+    <p>これは、
+        <middleware>yahoo.co.jp</middleware>
+        へのリンクです。
+    </p>
+    <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
+    <table>
+        <form action="/hello" method="post">
+            {{ csrf_field() }}
+            <tr>
+                <th>name:</th>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <th>mail:</th>
+                <td><input type="text" name="mail"></td>
+            </tr>
+            <tr>
+                <th>age:</th>
+                <td><input type="text" name="age"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="send"></td>
+            </tr>
+        </form>
+    </table>
 
 
 @endsection
