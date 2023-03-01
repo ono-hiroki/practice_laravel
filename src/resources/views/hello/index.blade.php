74bc7f3eb5575fd6ed39674b0ed3d092ab85c300
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-01 10:32:26 +0900

    フォームリクエストの試し

--- src/resources/views/hello/index.blade.php
+++ src/resources/views/hello/index.blade.php
@@ -37,10 +37,10 @@
                 <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
             @endif
             <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
-            @if ($errors->has('email'))
-                <tr><th>ERROR</th><td>{{$errors->first('email')}}</td></tr>
+            @if ($errors->has('mail'))
+                <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
             @endif
-            <tr><th>mail: </th><td><input type="text" name="email" value="{{old('email')}}"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
             @if ($errors->has('age'))
                 <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
             @endif
