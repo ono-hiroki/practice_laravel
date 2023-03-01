3d6be6941fd8a6ded0c4555378c0c5128bcc007a
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-01 11:05:11 +0900

    クエリのバリデーション

--- src/app/Http/Controllers/HelloController.php
+++ src/app/Http/Controllers/HelloController.php
@@ -11,7 +11,17 @@ class HelloController extends Controller
 {
     public function index(Request $request, Response $response)
     {
-        return view('hello.index', ['msg' => 'フォームを入力：']);
+        $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'pass' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $msg = 'クエリーに問題があります。';
+        } else {
+            $msg = 'ID/PASSを受け付けました。フォームを入力下さい。';
+        }
+
+        return view('hello.index', ['msg' => $msg]);
     }
 
     public function post(Request $request)
